b05a2e5e809643d0f779ced3843b2bbebf6225a7
brittatoresson <britta_toresson@hotmail.com>
2022-11-04 09:37:29 +0100

    config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 //DENNA NYCKEL BEHÖVER UPPDATERAS FÖR ATT FUNGERA, HÄMTA HÄR https://developer.spotify.com/console/get-search-item/
 
 export const token =
-  "BQAjx-RSeQkQ0G7v3RWHtnPFhGIEAFKzTwKE5l1-POfMuzWojJppDBJQGEISrfs2jZjSZakqlyfHvt1xRup5E-yJyr9aO0MxnU6MbSlcRMGUUOicM2TZAjYu9QyTtDqT4KjDcMGz1SMglDjgsxeShjhcq_I_Sm4_Z64hfuUGjMB7pLE_j7uSdofb5XFN4IsmxwybcU76";
+  "BQADgUd8EcmjM87SJrU2-e9dZO_p-HvZGbUSgpG6zn2BV1ii41VdOY9Ebow9ePcWinUq4zTS83F6LjoLIgJ2cVzJI-jlxeAtF6BrI1UZpkENoErogN0SIZzLR9vB736wEseg-NTr2ZpXUc92vJIgOpUlLUgivSdMIIfDBwlETeUybQwcQk4oekoGhlRpg3RhMctAlcbh";
 
 export const options = {
   method: "GET",
