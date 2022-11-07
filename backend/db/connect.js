860a6089be275785247db742f2a6aba9518c6032
brittatoresson <britta_toresson@hotmail.com>
2022-11-07 08:35:06 +0100

    server

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,8 +1,5 @@
 const { MongoClient } = require("mongodb");
-// let he = "mongodb+srv://cluster0.jy6hhtd.mongodb.net/myFirstDatabase";
-let hej = "mongodb+srv://Britta:britta@cluster0.jy6hhtd.mongodb.net/test";
-// const db = hej;
-const db = process.env.NODE_ENV;
+const db = process.env.ATLAS_URI;
 const client = new MongoClient(db, {
   useNewUrlParser: true,
   useunifiedtopology: true,
