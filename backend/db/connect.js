1100e6cb1ee3992a8e0ee201802b06d63aa23897
brittatoresson <britta_toresson@hotmail.com>
2022-11-07 11:13:23 +0100

    connect

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,9 +1,12 @@
 const { MongoClient } = require("mongodb");
 require("dotenv").config({ path: "./config.env" });
-const db =
-  process.env.ATLAS_URI ||
-  "mongodb+srv://Britta:britta@cluster0.jy6hhtd.mongodb.net/test";
-const client = new MongoClient(db, {
+const Db = process.env.ATLAS_URI;
+
+// const db =
+//   process.env.ATLAS_URI ||
+//   "mongodb+srv://Britta:britta@cluster0.jy6hhtd.mongodb.net/test";
+
+const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useunifiedtopology: true,
 });
